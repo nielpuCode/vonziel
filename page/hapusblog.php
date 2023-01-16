1bdbe33a39c6abf112356cade0787aa1ba1d80a4
danielmw <danielmw101@gmail.com>
2023-01-16 09:30:06 +0700

    giving margin top to p tag in hapus.blog

--- page/hapusblog.php
+++ page/hapusblog.php
@@ -28,8 +28,9 @@ if(isset($_POST["delete"])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Von Ziel</title>
@@ -38,8 +39,11 @@ if(isset($_POST["delete"])){
     <script src="page.js"></script>
     <script src="https://kit.fontawesome.com/c9a949b192.js" crossorigin="anonymous"></script>
 
-    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet">
 </head>
+
 <body>
     <div class="navbar">
         <label for="check">
@@ -48,22 +52,25 @@ if(isset($_POST["delete"])){
         </label>
 
         <input type="checkbox" id="check" name="check">
-        
+
         <ul style="z-index: 10;">
             <li id="firstlist"><a href="../index.php">Home</a></li>
             <li><a href="blog.php">Blog</a></li>
             <li id="lastlist"><a href="createnew.php">Write New</a></li>
         </ul>
     </div>
-    
+
     <div class="hapusblog">
         <form action="hapusblog.php" method="POST">
             <input style="margin-bottom: 0;" type="text" placeholder="your email" name="email" id="email">
-            <p style="text-align: center; font-style: italic; font-size: 15px; margin-bottom: 20px;">This action cannot be undone!</p>
+            <p style="text-align: center; font-style: italic; font-size: 15px; margin-bottom: 20px; margin-top: 10px;">
+                This
+                action cannot be undone!</p>
             <input id="id" name="id" type="text" value="<?php echo $_GET["id"];?>" placeholder="id blog">
 
             <button type="submit" name="delete">Delete</button>
         </form>
     </div>
 </body>
+
 </html>
